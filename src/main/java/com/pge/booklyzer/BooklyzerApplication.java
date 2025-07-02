26fb6ad2bad0b4f7dff08fb3f295839ce135c9bb
Deivide Duarte <deivideduarte@outlook.com>
2025-07-02 17:42:18 -0300

    Initializer

---
+++ src/main/java/com/pge/booklyzer/BooklyzerApplication.java
@@ -0,0 +1,13 @@
+package com.pge.booklyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklyzerApplication.class, args);
+	}
+
+}
